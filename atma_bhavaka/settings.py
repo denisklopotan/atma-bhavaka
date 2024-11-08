4ec0813be78c66b2590d39772bc884a0546babea
denisklopotan <denisklopotan@gmail.com>
2024-11-08 22:27:20 +0000

    Remove SECRET_KEY from commit history with BFG, then clean & hard reset

--- atma_bhavaka/settings.py
+++ atma_bhavaka/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # When working in production SECRET_KEY will be retrieved from env.py, else from enviroment variables (VAR):
+
 if os.path.isfile('env.py'):
     import env
 else:
