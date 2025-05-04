cd428d59d10f38ddd5dc38bab09191a2cb8d1179
denisklopotan <denisklopotan@gmail.com>
2025-05-04 19:56:46 +0000

    Update navbar menu hover color

--- atma_bhavaka/settings.py
+++ atma_bhavaka/settings.py
@@ -30,9 +30,11 @@ else:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
+    '8000-denisklopot-atmabhavaka-q3ovdhridwc.ws-eu118.gitpod.io',
+    'atma-bhavaka-2a6f708e729b.herokuapp.com',
     'www.atma-bhavaka.com',
     '.atma-bhavaka.com',
     'atma-bhavaka.com',
